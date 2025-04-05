ab5f6d9222aae7f78bcea1bb5e3991e609708151
harshsingh <harsh220902@gmail.com>
2025-04-05 18:00:58 +0530

    feeds aggregation

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/AarnoStormborn/go-RSS-aggregator/internal/database"
 	"github.com/go-chi/chi"
@@ -37,9 +38,14 @@ func main() {
 		log.Fatal("Cannot connect to Database")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(
+		db, 10, time.Minute,
+	)
 
 	router := chi.NewRouter()
 
